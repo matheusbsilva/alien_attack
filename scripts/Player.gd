5c178b8e1a550085579c4143779070751c4ec5f0
matheusbsilva <matheusbattista@hotmail.com>
2024-05-30 17:03:42 -0300

    Initial commit

---
+++ scripts/Player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var speed = 350
+
+func _physics_process(delta):
+	velocity = Vector2(0, 0)
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+
+	move_and_slide()
+	crop_position()
+	
+	
+func crop_position():
+	var w_size = get_viewport_rect().size
+	global_position = global_position.clamp(Vector2(0,0), w_size)
