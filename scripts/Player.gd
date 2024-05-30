ae0b5122c01f69c77071328794f9a1dac74148b7
matheusbsilva <matheusbattista@hotmail.com>
2024-05-30 17:21:27 -0300

    Add rocket scene

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,6 +3,15 @@ extends CharacterBody2D
 var speed = 350
 
 func _physics_process(delta):
+	process_movement_input()
+	move_and_slide()
+	crop_position()
+	
+func crop_position():
+	var w_size = get_viewport_rect().size
+	global_position = global_position.clamp(Vector2(0,0), w_size)
+	
+func process_movement_input():
 	velocity = Vector2(0, 0)
 	if Input.is_action_pressed("move_up"):
 		velocity.y = -speed
@@ -12,11 +21,3 @@ func _physics_process(delta):
 		velocity.x = -speed
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
-
-	move_and_slide()
-	crop_position()
-	
-	
-func crop_position():
-	var w_size = get_viewport_rect().size
-	global_position = global_position.clamp(Vector2(0,0), w_size)
